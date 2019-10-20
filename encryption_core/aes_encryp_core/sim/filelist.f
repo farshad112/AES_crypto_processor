32a9433e26da3b1a6c3fe5bfc8fdee2e144b7071
Farshad <farshad112@gmail.com>
2019-10-20 13:36:58 +0600

    added filelist

---
+++ encryption_core/aes_encryp_core/sim/filelist.f
@@ -0,0 +1,16 @@
++incdir+../../sbox/rtl/
++incdir+../../aes_key_expander/rtl
++incdir+../rtl
++incdir+../tb
+
+// include rtl files
+../../sbox/rtl/sbox.sv
+../rtl/aes_en_core.sv
+
+../../aes_key_expander/rtl/key_gen_sbox.sv
+../../aes_key_expander/rtl/aes_key_expander.sv
+
+
+// include tb files
+../tb/aes_en_core_tb.sv
+
