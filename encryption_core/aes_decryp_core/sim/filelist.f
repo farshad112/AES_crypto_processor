faa97ec9b9effac603850207cfef381555159dd3
Farshad <farshad112@gmail.com>
2019-10-24 09:50:02 +0600

    added filelist for running aes_decryption core simulation

---
+++ encryption_core/aes_decryp_core/sim/filelist.f
@@ -0,0 +1,16 @@
++incdir+../../isbox/rtl/
++incdir+../../aes_key_expander/rtl
++incdir+../rtl
++incdir+../tb
+
+// include rtl files
+../../isbox/rtl/isbox.sv
+../rtl/aes_decryp_core.sv
+
+../../aes_key_expander/rtl/key_gen_sbox.sv
+../../aes_key_expander/rtl/aes_key_expander.sv
+
+
+// include tb files
+../tb/aes_decryp_core_tb.sv
+
